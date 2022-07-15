3056c5c113d0a6d8ba8dc8c89818f7659f998bd2
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2022-07-15 20:26:25 +0300

    Rewrite WordsCount component's render

--- src/components/TypingSettings/WordsCount/WordsCount.tsx
+++ src/components/TypingSettings/WordsCount/WordsCount.tsx
@@ -5,39 +5,28 @@ import s from "./styles.module.scss";
 export const WordsCount = () => {
   const { wordsCount, setWordsCount } = useContext(TypingContext);
 
+  const renderWordsCountStates = () => {
+    const values = [15, 35, 50];
+
+    return values.map((count, index) => (
+      <p
+        key={index}
+        className={
+          wordsCount === count
+            ? s["words-count__value--active"]
+            : s["words-count__value"]
+        }
+        onClick={() => setWordsCount(count)}
+      >
+        {count}
+      </p>
+    ));
+  };
+
   return (
     <div className={s["words-count"]}>
       <h6>Words number: </h6>
-      <p
-        className={
-          wordsCount === 15
-            ? s["words-count__value--active"]
-            : s["words-count__value"]
-        }
-        onClick={() => setWordsCount(15)}
-      >
-        15
-      </p>
-      <p
-        className={
-          wordsCount === 35
-            ? s["words-count__value--active"]
-            : s["words-count__value"]
-        }
-        onClick={() => setWordsCount(35)}
-      >
-        35
-      </p>
-      <p
-        className={
-          wordsCount === 50
-            ? s["words-count__value--active"]
-            : s["words-count__value"]
-        }
-        onClick={() => setWordsCount(50)}
-      >
-        50
-      </p>
+      {renderWordsCountStates()}
     </div>
   );
 };
